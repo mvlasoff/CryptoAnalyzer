18b9d6f2aa29bff4bd14b3e4a9781d3d18262a51
mvlasoff <hednight@gmail.com>
2022-03-10 21:54:54 -0500

    3.10 7. Runner changes.

--- src/main/java/ru/javarush/vlasov/cryptoanalyzer/Runner.java
+++ src/main/java/ru/javarush/vlasov/cryptoanalyzer/Runner.java
@@ -2,6 +2,6 @@ package ru.javarush.vlasov.cryptoanalyzer;
 
 public class Runner {
     public static void main(String[] args) {
-        System.out.println("Hello, maven!");
+        System.out.println("Hello, maven!!");
     }
 }
